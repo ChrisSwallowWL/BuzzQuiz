4ebccdd811639811f6b5b6d876d0d036e1e0abb3
Chris Swallow <chris.swallow@worldline.com>
2022-11-23 08:45:11 +0000

    Refactored sound and added wait time options. Load questions from JSON file.

--- quizGame.py
+++ quizGame.py
@@ -1,20 +1,29 @@
 from __future__ import print_function
-from BuzzController import BuzzController
+
 import time
 import _thread
+import json
+from os import path
+from BuzzController import BuzzController
 from random import shuffle
 from playsound import playsound
 
-# Define the questions here. Correct answer first.
-import_questions = [
-    {"question": "What is the capital of Australia", "answers": ["Canberra", "Sydney", "Hobart", "Melbourne"]},
-    {"question": "What is the capital of Japan", "answers": ["Tokyo", "Horoshima", "Osaka", "Kyoto"]},
-]
+
+def play_sound(sound_file, wait=0):
+    _thread.start_new_thread(playsound, (path.abspath(sound_file),))
+    time.sleep(wait)
+
+
+def load_questions(file):
+    return json.load(file)
+
+
 questions = []
 score = [0, 0, 0, 0]
 
 # Import and shuffle the questions
-for question in import_questions:
+question_file = open('Questions/Round1.json')
+for question in load_questions(question_file):
     buttons = ["blue", "orange", "green", "yellow"]
     new_answer = {}
     shuffle(buttons)
@@ -43,20 +52,20 @@ for question in questions:
         controller = buzz.controller_get_first_pressed("red", available_controllers)
         buzz.light_blinking = False
         buzz.light_set(controller, True)
-        playsound('Sounds/buzzer.wav')
+        play_sound('Sounds/buzzer.wav', 1)
         time.sleep(0.5)
 
         while True:
             button = buzz.get_button_pressed(controller)
             if button and button != "red":
                 if button == question["correct"]:
-                    playsound('Sounds/bell.wav')
+                    play_sound('Sounds/bell.wav', 1)
                     print("Controller " + str(controller + 1) + " was correct")
                     question_answered = True
                     score[controller] += 1
                     break
                 elif button.capitalize() in available_answers:
-                    playsound('Sounds/wrong.wav')
+                    play_sound('Sounds/wrong.wav', 1)
                     print("Sorry incorrect answer")
                     available_controllers.remove(controller)
                     available_answers.remove(button.capitalize())
