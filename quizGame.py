9ba22d93e65fb4c85d741c7e6d82492d68bad50f
Chris Swallow <fuzzyjack76@gmail.com>
2022-12-02 19:52:52 +0000

    Latest tweaks.

--- quizGame.py
+++ quizGame.py
@@ -39,6 +39,8 @@ def load_questions(file):
                 new_answer["correct"] = buttons[i]
             new_answer[buttons[i]] = q["answers"][i]
         questions.append(new_answer)
+        if question_type == "music":
+            tracks.append(q["track"])
 
 
 def reset():
@@ -64,18 +66,26 @@ def reset():
     startButton['state'] = NORMAL
 
     questionLabel.configure(text="Press Start to Begin")
-    blue_answer.configure(text="")
-    orange_answer.configure(text="")
-    green_answer.configure(text="")
-    yellow_answer.configure(text="")
+    set_image(blue_answer, "Pictures/Blue.jpg")
+    set_image(orange_answer, "Pictures/Orange.jpg")
+    set_image(green_answer, "Pictures/Green.jpg")
+    set_image(yellow_answer, "Pictures/Yellow.jpg")
 
 
-def set_label(label, text, image):
+def set_label(label, text, colour):
+    img = Image.open("Pictures/" + colour + ".jpg")
+    resized_image = img.resize((750, 420), Image.Resampling.LANCZOS)
+    image = ImageTk.PhotoImage(resized_image)
     label.config(image=image, text=text, width=740, height=450)
     label.image = image
 
 
-def set_image(label, image, width, height):
+def set_image(label, file):
+    img = Image.open(file)
+    width = int(img.size[0])
+    height = int(img.size[1])
+    resized_image = img.resize((width, height), Image.Resampling.LANCZOS)
+    image = ImageTk.PhotoImage(resized_image)
     label.configure(text="", image=image, width=width, height=height)
     label.image = image
 
@@ -93,6 +103,10 @@ def start():
     if startButton['text'] == "Finish":
         reset()
         return
+
+    if question_type == "music":
+        questionLabel.configure(text="Listen....")
+        play_sound(tracks[current_question], 30)
 
     # Output the question
     questionLabel.configure(text=questions[current_question]["question"])
@@ -124,43 +138,25 @@ def update_score(controller, points):
 
 
 def set_answers(answer_colour, answer):
-    if question_type == "text":
+    if question_type == "image":
         if answer_colour == "Blue":
-            img = Image.open("Pictures/blue.jpg")
-            resized_image = img.resize((750, 420), Image.Resampling.LANCZOS)
-            image = ImageTk.PhotoImage(resized_image)
-            set_label(blue_answer, answer, image)
+            set_image(blue_answer, answer)
         if answer_colour == "Orange":
-            img = Image.open("Pictures/orange.jpg")
-            resized_image = img.resize((750, 420), Image.Resampling.LANCZOS)
-            image = ImageTk.PhotoImage(resized_image)
-            set_label(orange_answer, answer, image)
+            set_image(orange_answer, answer)
         if answer_colour == "Green":
-            img = Image.open("Pictures/green.jpg")
-            resized_image = img.resize((750, 420), Image.Resampling.LANCZOS)
-            image = ImageTk.PhotoImage(resized_image)
-            set_label(green_answer, answer, image)
+            set_image(green_answer, answer)
         if answer_colour == "Yellow":
-            img = Image.open("Pictures/yellow.jpg")
-            resized_image = img.resize((750, 420), Image.Resampling.LANCZOS)
-            image = ImageTk.PhotoImage(resized_image)
-            set_label(yellow_answer, answer, image)
+            set_image(yellow_answer, answer)
         print(answer_colour + " " + answer)
-
-    elif question_type == "image":
-        image = Image.open(answer)
-        width = int(image.size[0])
-        height = int(image.size[1])
-        resized_image = image.resize((width, height), Image.Resampling.LANCZOS)
-        img = ImageTk.PhotoImage(resized_image)
+    else:
         if answer_colour == "Blue":
-            set_image(blue_answer, img, width, height)
+            set_label(blue_answer, answer, answer_colour)
         if answer_colour == "Orange":
-            set_image(orange_answer, img, width, height)
+            set_label(orange_answer, answer, answer_colour)
         if answer_colour == "Green":
-            set_image(green_answer, img, width, height)
+            set_label(green_answer, answer, answer_colour)
         if answer_colour == "Yellow":
-            set_image(yellow_answer, img, width, height)
+            set_label(yellow_answer, answer, answer_colour)
         print(answer_colour + " " + answer)
 
 
@@ -169,12 +165,17 @@ def wait_for_buzz(question_number):
     available_answers = ["Blue", "Orange", "Green", "Yellow"]
     available_controllers = [0, 1, 2, 3]
 
+    # Display the answers for the current question
     question = questions[question_number]
+    for answer in available_answers:
+        set_answers(answer, question[answer.lower()])
+
+    # Call this to start so that it resets any buzzers pushed before the question is set
+    buzz.get_button_pressed(0)
+
     while not question_answered:
 
-        for answer in available_answers:
-            set_answers(answer, question[answer.lower()])
-
+        # Start the lights blinking and wait for a button press
         _thread.start_new_thread(buzz.light_blink, (available_controllers,))
         controller = buzz.controller_get_first_pressed("red", available_controllers)
         buzz.light_blink_stop()
@@ -182,9 +183,11 @@ def wait_for_buzz(question_number):
         play_sound('Sounds/buzzer.wav', 1)
         time.sleep(0.5)
 
+        countdown = int(timeLabel['text'])
         while True:
-            # TODO: Start a timer once they buzz in which they have to press the option (3 secs)
             button = buzz.get_button_pressed(controller)
+            buzz.light_blink_stop()
+
             if button and button != "red":
                 if button == question["correct"]:
                     play_sound('Sounds/bell.wav', 1)
@@ -197,8 +200,14 @@ def wait_for_buzz(question_number):
                     play_sound('Sounds/wrong.wav', 1)
                     print("Sorry incorrect answer")
                     available_controllers.remove(controller)
-                    available_answers.remove(button.capitalize())
+                    # available_answers.remove(button.capitalize())
                     break
+            time.sleep(0.2)
+            countdown -= 0.2
+            if countdown < 0:
+                play_sound('Sounds/wrong.wav', 1)
+                available_controllers.remove(controller)
+                break
         buzz.light_set(controller, False)
     time.sleep(1)
 
@@ -206,6 +215,7 @@ def wait_for_buzz(question_number):
 # Main Program
 questions = []
 question_type = ""
+tracks = []
 current_round = 1
 current_question = 0
 score = [0, 0, 0, 0]
@@ -256,87 +266,75 @@ team4_score.grid(row=1, column=0)
 
 
 def time_minus():
-    return
+    current_time = int(timeLabel['text'])
+    new_time = current_time - 1
+    timeLabel.configure(text=new_time)
 
 
 def time_plus():
-    return
+    current_time = int(timeLabel['text'])
+    new_time = current_time + 1
+    timeLabel.configure(text=new_time)
 
 
 def time_reset():
-    return
+    timeLabel.configure(text=3)
 
 
-def set_time_string(string):
-    global timeString, timeLabel
-
-    try:
-        timeString.set(string)  # TODO Why does this throw an error?
-    # print "successfully set to: "+str(string))
-    except AttributeError:
-        print("Failed to set to: " + string)
-        timeString = StringVar(timeLabel)
-        timeLabel.config(textvariable=timeString)
-        set_time_string(string)
-
-
-timeString = StringVar()
-# # setTimeString("Ready to Start Countdown!")
 timeFrame = Frame(root, borderwidth=5)
 timeFrame.grid(row=1, column=0, columnspan=4)
 Label(timeFrame, text="Time:", justify="right", font=medium_font).grid(row=0, column=0, sticky='e')
 minusButton = Button(timeFrame, text="-", width=1, command=time_minus)
 minusButton.grid(row=0, column=1, padx=10)
-timeLabel = Label(timeFrame, textvariable=timeString, width=2, justify="center", font=medium_font)
+timeLabel = Label(timeFrame, text=3, width=2, justify="center", font=medium_font)
 timeLabel.grid(row=0, column=2)
 plusButton = Button(timeFrame, text="+", width=1, command=time_plus)
 plusButton.grid(row=0, column=3, padx=10)
 falseStartButton = Button(timeFrame, text="Reset", width=2, command=time_reset)
 falseStartButton.grid(row=0, column=4)
-set_time_string("3")
 
 # Label displaying question # and associated buttons
 questionFrame = Frame(root, borderwidth=5)
-questionFrame.grid(row=2, column=0, columnspan=4)
+questionFrame.grid(row=2, column=0, columnspan=4, rowspan=2)
 Label(questionFrame, justify="center").grid(row=0, column=0)
 questionLabel = Label(questionFrame, justify="center", font=big_font, wraplength=1500)
 questionLabel.grid(row=0, column=2)
 
 blue_frame = Frame(root, width=750, height=400, borderwidth=8, background="blue")
-blue_frame.grid(row=3, column=0, columnspan=2, rowspan=2)
+blue_frame.grid(row=4, column=0, columnspan=2, rowspan=2)
 blue_frame.grid_propagate(False)
 blue_answer = Label(blue_frame, width=56, height=19, anchor="center", compound="center", font=big_font, fg="orange",
                     background="blue", wraplength=700)
 blue_answer.grid(row=0, column=0, columnspan=1)
 
 orange_frame = Frame(root, width=750, height=400, borderwidth=8, background="orange")
-orange_frame.grid(row=3, column=2, columnspan=2, rowspan=2)
+orange_frame.grid(row=4, column=2, columnspan=2, rowspan=2)
 orange_frame.grid_propagate(False)
 orange_answer = Label(orange_frame, width=56, height=19, compound="center", font=big_font, fg="blue",
                       background="orange", wraplength=700)
 orange_answer.grid(row=0, column=1, columnspan=1)
 
 green_frame = Frame(root, width=750, height=400, borderwidth=8, background="green")
-green_frame.grid(row=5, column=0, columnspan=2, rowspan=2)
+green_frame.grid(row=6, column=0, columnspan=2, rowspan=2)
 green_frame.grid_propagate(False)
 green_answer = Label(green_frame, width=56, height=19, compound="center", font=big_font, fg="white", background="green",
                      wraplength=700)
 green_answer.grid(row=1, column=0, columnspan=1)
 
 yellow_frame = Frame(root, width=750, height=400, borderwidth=8, background="yellow")
-yellow_frame.grid(row=5, column=2, columnspan=2, rowspan=2)
+yellow_frame.grid(row=6, column=2, columnspan=2, rowspan=2)
 yellow_frame.grid_propagate(False)
 yellow_answer = Label(yellow_frame, width=56, height=19, compound="center", font=big_font, fg="purple",
                       background="yellow", wraplength=700)
 yellow_answer.grid(row=1, column=1, columnspan=1)
 
 resetButton = Button(root, text="Reset", justify="center", width=20, height=2, command=reset, font=medium_font)
-resetButton.grid(row=7, column=0)
+resetButton.grid(row=8, column=0)
 
 startButton = Button(root, text="Start", justify="center", width=20, height=2, command=start, font=medium_font)
-startButton.grid(row=7, column=3)
+startButton.grid(row=8, column=3)
 
-rounds = 3
+rounds = 5
 
 # Initialise the buzzer and turn the lights off
 buzz = BuzzController()
